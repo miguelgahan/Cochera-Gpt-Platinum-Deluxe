cc4a9d6860c324cb585e27f86a1affd527d98b8c
miguel <gahanmiguel@gmail.com>
2023-04-11 21:12:37 -0300

    primer commit

--- src/main/java/curso/cochera/servicios/servicioTest.java
+++ src/main/java/curso/cochera/servicios/servicioTest.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class servicioTest {
 
-    Descuentos testRepositorio  (String codigo){};
+    //Descuentos testRepositorio  (String codigo){};
 
 
 }
